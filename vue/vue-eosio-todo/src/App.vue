8b1d934ddc98a5902f2a93a0db7d944004f3744f
Jack Tanner <jack@jackandtheblockstalk.com>
2020-01-22 00:18:04 +0100

    created header and body components

--- vue/vue-eosio-todo/src/App.vue
+++ vue/vue-eosio-todo/src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader projectName="Todo list"/>
+    <TheBody accountName="jacko"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+import TheBody from './components/TheBody.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TheHeader,
+    TheBody
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ vue/vue-eosio-todo/src/components/TheBody.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      This is the body
+      Account name: {{accountName}}
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        accountName: String
+    },
+    data() {
+        return {
+            todoItems: []
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
