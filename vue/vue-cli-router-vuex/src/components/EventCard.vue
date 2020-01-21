4c42b9b735a27ba513530a6fc74096e2101a6eda
Jack Tanner <jack@jackandtheblockstalk.com>
2020-01-21 19:15:27 +0100

    EventCard

---
+++ vue/vue-cli-router-vuex/src/components/EventCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Heading</h1>
+        <ul>
+            <li>item 1</li>
+            <li>{{quote}}</li>
+        </ul>
+    </div>  
+</template>
+
+<script>
+export default {
+    name: "EventCard",
+    data() {
+        return {
+            quote: "this is a quote"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- vue/vue-cli-router-vuex/src/views/Home.vue
+++ vue/vue-cli-router-vuex/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <EventCard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    EventCard
   }
 };
 </script>
