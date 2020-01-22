24f23df62ed65f4d366caa256b89187acfb2e81e
Jack Tanner <jack@jackandtheblockstalk.com>
2020-01-22 11:08:45 +0100

    removed bootstrap

--- vue/vue-eosio-todo/src/components/TheHeader.vue
+++ vue/vue-eosio-todo/src/components/TheHeader.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ projectName }}</h1>
-    <b-navbar type="dark" variant="dark">
-      <b-nav-item href="#">Link</b-nav-item>
-      
-      <b-nav-item-dropdown text="User" right>
-        <b-dropdown-item href="#">Account</b-dropdown-item>
-        <b-dropdown-item href="#">Settings</b-dropdown-item>
-      </b-nav-item-dropdown>
-    </b-navbar>
   </div>
 </template>
 
@@ -22,18 +14,4 @@ export default {
 </script>
 
 <style scoped>
-/* h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-} */
 </style>
--- vue/vue-eosio-todo/src/main.js
+++ vue/vue-eosio-todo/src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
-// Install BootstrapVue
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   render: h => h(App),
