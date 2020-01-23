8ce34d34c18952732b8fc009510243c7d1c1950e
Jack Tanner <jack@jackandtheblockstalk.com>
2020-01-23 12:09:37 +0100

    finished scatter working

--- vue/vue-eosio-todo/client/src/scatter.js
+++ vue/vue-eosio-todo/client/src/scatter.js
@@ -4,18 +4,10 @@ import {JsonRpc, Api} from 'eosjs'
 
 ScatterJS.plugins( new ScatterEOS() );
 
-// const network = {
-//     blockchain: 'eos',
-//     protocol: 'https',
-//     host: 'eos-studio.api.dfuse.dev',
-//     port: 443,
-//     chainId: 'bc31c358a5aaafb5f7ad73a2ef85625f67fe9dc027f8c441fc272027d53f00f6'
-// }
-
 const network = ScatterJS.Network.fromJson({
     blockchain:'eos',
-    chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    host:'nodes.get-scatter.com',
+    chainId:'bc31c358a5aaafb5f7ad73a2ef85625f67fe9dc027f8c441fc272027d53f00f6',
+    host:'eos-studio.api.dfuse.dev',
     port:443,
     protocol:'https'
 });
@@ -39,14 +31,24 @@ export default {
         const rpc = new JsonRpc(network.fullhost());
         eos = ScatterJS.eos(network, Api, {rpc});
 
-        const contract = await eos.contract("eosio.token");
-
-        const trx = await contract.transfer({
-            from: "dablockstalk",
-            to: "b1",
-            quantity: "1.0000 EOS",
-            memo: ""
-        })
+        const trx = await eos.transact({
+            actions: [{
+                account: 'eosio.token',
+                name: 'transfer',
+                authorization: [{
+                actor: 'dablockstalk',
+                permission: 'active',
+            }],
+            data: {
+                from: 'dablockstalk',
+                to: 'b1',
+                quantity: '0.0001 EOS',
+                memo: '',
+            },
+            }]}, {
+                blocksBehind: 3,
+                expireSeconds: 30,
+          })
     },
     logout: async function() {
         ScatterJS.logout()
