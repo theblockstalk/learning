83ea1278884a0324fad317e4aed07510322ad545
Jack Tanner <jack@jackandtheblockstalk.com>
2020-02-06 14:42:11 +0100

    installed bootstrap

--- vue/vue-eosio-todo/client/src/main.js
+++ vue/vue-eosio-todo/client/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   store,
