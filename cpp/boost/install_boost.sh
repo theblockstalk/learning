dbd2e5d31bf34ac2a4533d3767f445de1199167a
Jack Tanner <jack@jackandtheblockstalk.com>
2020-03-26 10:28:32 +0100

    boost build

--- cpp/boost/install_boost.sh
+++ cpp/boost/install_boost.sh
@@ -2,4 +2,15 @@
 
 # sudo apt install libboost-all-dev
 
+cd /usr/local
 wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.bz2
+sudo tar --bzip2 -xf boost_1_72_0.tar.bz2
+sudo rm boost_1_72_0.tar.bz2
+cd ./boost_1_72_0
+sudo ./bootstrap.sh
+sudo ./b2
+
+# The following directory should be added to compiler include paths:
+#     /usr/local/boost_1_72_0
+# The following directory should be added to linker library paths:
+#     /usr/local/boost_1_72_0/stage/lib
--- cpp/boost/main.cpp
+++ cpp/boost/main.cpp
@@ -1,9 +1,13 @@
+#include <boost/lambda/lambda.hpp>
 #include <iostream>
-#include <boost/whatever.hpp>
+#include <iterator>
+#include <algorithm>
 
-using std::cout, std::endl;
-
-int main(int argc, char **argv)
+int main()
 {
-    return 0;
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
 }
