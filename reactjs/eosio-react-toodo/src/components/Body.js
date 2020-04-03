a258101de9c1a97a0144cb8923dd64454c7d06b6
Jack Tanner <jack@jackandtheblockstalk.com>
2020-04-03 16:06:58 +0200

    added material ui

--- reactjs/eosio-react-toodo/src/components/Body.js
+++ reactjs/eosio-react-toodo/src/components/Body.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 class Body extends React.Component {
   render() {
     return (
       <div>
+
           This is the body
+          <br/>
+          <Button variant="contained" color="primary">
+              Hello World
+            </Button>
       </div>
     );
   }
