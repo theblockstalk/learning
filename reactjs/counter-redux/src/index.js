ed34dedc70fc3875b306eadfc3929a8e504d71fa
Jack Tanner <jack@jackandtheblockstalk.com>
2020-04-09 20:24:11 +0200

    added store

--- reactjs/counter-redux/src/index.js
+++ reactjs/counter-redux/src/index.js
@@ -1,12 +1,16 @@
+// https://daveceddia.com/redux-tutorial/
 import React from "react";
 import { render } from "react-dom";
 import Counter from "./Counter";
+import { Provider } from 'react-redux';
+import store from './store';
 import "./index.css";
 
+
 const App = () => (
-  <div>
+  <Provider store={store}>
     <Counter />
-  </div>
+  </Provider>
 );
 
 render(<App />, document.getElementById("root"));
---
+++ reactjs/counter-redux/src/store.js
@@ -0,0 +1,34 @@
+import { createStore } from 'redux';
+
+const initialState = {
+    count: 0
+  };
+  
+  function reducer(state = initialState, action) {
+    console.log('reducer', state, action);
+  
+    switch(action.type) {
+      case 'INCREMENT':
+        return {
+          count: state.count + 1
+        };
+      case 'DECREMENT':
+        return {
+          count: state.count - 1
+        };
+      case 'RESET':
+        return {
+          count: 0
+        };
+      default:
+        return state;
+    }
+  }
+  
+  const store = createStore(reducer);
+  store.dispatch({ type: "INCREMENT" });
+  store.dispatch({ type: "INCREMENT" });
+  store.dispatch({ type: "DECREMENT" });
+  store.dispatch({ type: "RESET" });  
+
+  export default store;
