a2a7a7e2a6264063357d3d69a9bb935a62686889
Jack Tanner <jack@jackandtheblockstalk.com>
2020-06-25 18:03:48 +0200

    updated install for binaries and other scripts

--- hyperledger/fabric/install.sh
+++ hyperledger/fabric/install.sh
@@ -6,12 +6,30 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 
-# https://hyperledger-fabric.readthedocs.io/en/release-2.1/prereqs.html
+## https://hyperledger-fabric.readthedocs.io/en/release-2.1/prereqs.html
 
 
-# https://hyperledger-fabric.readthedocs.io/en/release-2.1/install.html
-cd ~/Documents/Git/Hyperledger
-git clone git@github.com:hyperledger/fabric.git #not necessary
+## https://hyperledger-fabric.readthedocs.io/en/release-2.1/install.html
+# Not necessary but can be useful to have code
+git clone git@github.com:hyperledger/fabric.git
+
+# Install fabric
 curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s./bootstrap.sh
-export PATH=~/Documents/Git/Hyperledger/fabric-samples/bin:$PATH
+sudo ln ./fabric-samples/test-network/bin/* /bin
 
+# Logsprout launcher
+# https://hyperledger-fabric.readthedocs.io/en/release-2.1/deploy_chaincode.html#setup-logspout-optional
+sudo ln ./fabric-samples/commercial-paper/organization/digibank/configuration/cli/monitordocker.sh /bin
+
+cd ./fabric-samples/test-network
+export FABRIC_CFG_PATH=$PWD/../config/
+
+# Name terminal (useful for tracking peers)
+export PS1="MagnetoCorp (Org1)$ "
+
+./network.sh down
+sudo rm organizations/fabric-ca/ordererOrg/msp/ -rf
+sudo rm organizations/fabric-ca/org1/msp/ -rf
+sudo rm organizations/fabric-ca/org2/msp/ -rf
+docker rm $(docker ps -a) -f
+./network.sh down
