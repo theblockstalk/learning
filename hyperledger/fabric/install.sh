9d7ba2ee8faabeea2ddf445829d56f21756547dc
Jack Tanner <jack@jackandtheblockstalk.com>
2020-07-01 18:53:12 +0200

    fix

--- hyperledger/fabric/install.sh
+++ hyperledger/fabric/install.sh
@@ -14,8 +14,8 @@ sudo apt autoremove -y
 git clone git@github.com:hyperledger/fabric.git
 
 # Install fabric
-curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s./bootstrap.sh
-sudo ln ./fabric-samples/test-network/bin/* /bin
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s
+sudo ln ./fabric-samples/bin/* /bin
 
 # Logsprout launcher
 # https://hyperledger-fabric.readthedocs.io/en/release-2.1/deploy_chaincode.html#setup-logspout-optional
@@ -27,6 +27,7 @@ export FABRIC_CFG_PATH=$PWD/../config/
 # Name terminal (useful for tracking peers)
 export PS1="MagnetoCorp (Org1)$ "
 
+# for deleting all test network data (even if it uses CAs)
 ./network.sh down
 sudo rm organizations/fabric-ca/ordererOrg/msp/ -rf
 sudo rm organizations/fabric-ca/org1/msp/ -rf
