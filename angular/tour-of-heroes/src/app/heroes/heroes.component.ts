7902d761479fdc26744be921d69cada6a6e590da
Jack Tanner <jack@jackandtheblockstalk.com>
2020-07-02 15:54:30 +0200

    add services

--- angular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,16 +13,22 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   }
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
