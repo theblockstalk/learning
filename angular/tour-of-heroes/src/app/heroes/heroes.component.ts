73acbe258497595dd9ac7306b357d18bee07775f
Jack Tanner <jack@jackandtheblockstalk.com>
2020-07-02 14:04:20 +0200

    display a list

--- angular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,6 +11,12 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
+  }
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   constructor() { }
