bad27067cfb6675b7787bddad4a1a1ca5a6a9b21
Jack Tanner <jack@jackandtheblockstalk.com>
2021-06-19 16:00:42 +0200

    Updates for shamirs

--- crypto/shamirs/index.js
+++ crypto/shamirs/index.js
@@ -1,6 +1,9 @@
 const sss = require('shamirs-secret-sharing');
+const base64js = require('base64-js')
 
 const secret = Buffer.from('secret key');
 const shares = sss.split(secret, { shares: 10, threshold: 4 });
+console.log(shares.map(share => base64js.fromByteArray(share)));
+
 const recovered = sss.combine(shares.slice(3, 7));
 console.log(recovered.toString());
