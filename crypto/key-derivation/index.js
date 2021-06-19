0847565e2f64c5da31dd94fc4657f1939b9c0cac
Jack Tanner <jack@jackandtheblockstalk.com>
2021-06-19 16:57:11 +0200

    Print memory usage

--- crypto/key-derivation/index.js
+++ crypto/key-derivation/index.js
@@ -10,6 +10,28 @@ function normalizeUnicodePassword(password) {
     return password.normalize('NFKC');
 }
 
+let previousMemory = {}, firstTime = true;
+function printMemory(msg = '') {
+    const used = process.memoryUsage();
+    console.log(msg === '' ? msg : '\n' + msg);
+    for (let key in used) {
+        if (!firstTime) {
+            console.log(`${key} ${Math.round((used[key] - previousMemory[key]) / 1024 / 1024 * 100) / 100} MB`);
+        }
+        previousMemory[key] = used[key];
+    }
+    firstTime = false;
+}
+
+function getMemory() {
+    const used = process.memoryUsage();
+    return used.rss;
+}
+
+function bytesToMegabytes(bytes) {
+    return Math.round(bytes / 1024 / 1024 * 100) / 100;
+}
+
 async function main() {
     const password = "Password123!";
     const salt = "abcdefg12345678";
@@ -20,9 +42,18 @@ async function main() {
     const normalizedPassword = normalizeUnicodePassword(password);
     const bufferPassword = Buffer.from(normalizedPassword);
 
-    const n = 1024, r = 80, p = 1;
-    const key = await scrypt.scrypt(bufferPassword, Buffer.from(salt), n, r, p, keyLengthBytes)
-    console.log(normalizedPassword, salt, n, r, p, key.length, base64js.fromByteArray(key));
+    // Parameters used for my crypto wallet
+    const n = 8192, r = 8, p = 1;
+    // Need to call once before measuring to initalize library codebase
+    let key = await scrypt.scrypt(bufferPassword, Buffer.from(salt), n, r, p, keyLengthBytes);
+    
+    const startTime = new Date();
+    key = await scrypt.scrypt(bufferPassword, Buffer.from(salt), n, r, p, keyLengthBytes);
+    const endTime = new Date();
+    const durationMilli = endTime.getTime() - startTime.getTime();
+    const memoryUsed = 128 * n * r * p / 1000;
+    
+    console.log(normalizedPassword, salt, n, r, p, key.length, base64js.fromByteArray(key), durationMilli + 'ms', memoryUsed + ' MB');
 }
 
 main();
