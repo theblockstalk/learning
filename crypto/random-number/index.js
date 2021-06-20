d941a8f7ac1bd6ddaaaa65fa166a3a9652c696fb
Jack Tanner <jack@jackandtheblockstalk.com>
2021-06-20 11:10:43 +0200

    random user entropy

--- crypto/random-number/index.js
+++ crypto/random-number/index.js
@@ -1,6 +1,20 @@
 const crypto = require('crypto');
-const base64js = require('base64-js')
+const base64js = require('base64-js');
+const prompt = require('prompt-sync')();
 
-// const browserRandom = window.crypto.getRandomValues();
-const nodeRandom = crypto.randomBytes(32);
-console.log(nodeRandom.length, base64js.fromByteArray(nodeRandom));
+async function main() {
+    let entropy = '';
+    for (let i = 0; i < 5; i++) {
+        const text = prompt(`Please enter text (${i+1}/5):`);
+        const time = new Date();
+        entropy += text + time.toISOString() + ' | ';
+    }
+
+    console.log('entropy', entropy);
+
+    // const browserRandom = window.crypto.getRandomValues();
+    const nodeRandom = crypto.randomBytes(32);
+    console.log(nodeRandom.length, base64js.fromByteArray(nodeRandom));
+}
+
+main();
