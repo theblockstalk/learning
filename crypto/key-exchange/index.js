de54af0ac65565644611dbaea5355f389513f21d
Jack Tanner <jack@jackandtheblockstalk.com>
2021-06-20 12:04:59 +0200

    three key exchange

--- crypto/key-exchange/index.js
+++ crypto/key-exchange/index.js
@@ -11,3 +11,19 @@ const shared2 = key2.derive(key1.getPublic());
 console.log('Both shared secrets are BN instances');
 console.log(shared1.toString(16));
 console.log(shared2.toString(16));
+
+const A = ec.genKeyPair();
+const B = ec.genKeyPair();
+const C = ec.genKeyPair();
+
+const AB = A.getPublic().mul(B.getPrivate())
+const BC = B.getPublic().mul(C.getPrivate())
+const CA = C.getPublic().mul(A.getPrivate())
+
+const ABC = AB.mul(C.getPrivate())
+const BCA = BC.mul(A.getPrivate())
+const CAB = CA.mul(B.getPrivate())
+
+console.log(ABC.getX().toString(16))
+console.log(BCA.getX().toString(16))
+console.log(CAB.getX().toString(16))
